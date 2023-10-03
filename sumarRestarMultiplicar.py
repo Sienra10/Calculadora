1edc4e91554aede77e7da8798ed27bb216e7b98a
Sergio Sienra <zx22student3213@colegiovalledelmiro.es>
2023-10-03 13:13:06 +0200

    modificar suma

--- sumarRestarMultiplicar.py
+++ sumarRestarMultiplicar.py
@@ -4,8 +4,8 @@ def sumar(a, b):
   return a + b
 
 if __name__ == "__main__":
-  num1 = float(input("Pon el primer número: "))
-  num2 = float(input("Pon el segundo número: "))
+  num1 = float(input("Ingresa el primer número: "))
+  num2 = float(input("Ingresa el segundo número: "))
   resultado = sumar(num1, num2)
   print("La suma es:", resultado)
 
