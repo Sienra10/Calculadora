e3ed299d22aab374f81653db0614740a255117c6
Sergio Sienra <zx22student3213@colegiovalledelmiro.es>
2023-10-03 10:21:48 +0200

    funcion resta

--- sumarRestarMultiplicar.py
+++ sumarRestarMultiplicar.py
@@ -8,3 +8,12 @@ if __name__ == "__main__":
   num2 = float(input("Pon el segundo número: "))
   resultado = sumar(num1, num2)
   print("La suma es:", resultado)
+
+def restar(a, b):
+  return a - b
+
+if __name__ == "__main__":
+  num1 = float(input("Ingrese el primer número: "))
+  num2 = float(input("Ingrese el segundo número: "))
+  resultado = restar(num1, num2)
+  print("La resta es:", resultado)
